92f21860e4a279e22c2517b789fd8e653f74788b
LW-Ho <leoleo41306@gmail.com>
2018-06-13 20:16:32 +0800

    fix bugs.

--- projects/white_Testing/border-router/slip-bridge.c
+++ projects/white_Testing/border-router/slip-bridge.c
@@ -175,10 +175,10 @@ output(void)
 
       #if haveArduino 
       {
-        uint8_t gasValue = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 37];
-        uint8_t gasAlarm = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 38];
-        uint8_t temperature = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 39];
-        uint8_t humidity = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 40];
+        uint8_t gasValue = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 36];
+        uint8_t gasAlarm = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 37];
+        uint8_t temperature = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 38];
+        uint8_t humidity = ((uint8_t *) (UIP_IP_BUF))[coap_packet_start_location + 39];
 
         PRINTF("Temperature: %d.%dC\n", temperature / 100, temperature % 100);
         PRINTF("Rel. humidity: %d.%d%%\n", humidity / 100, humidity % 100);
