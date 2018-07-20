834068cd71ddee7b5b5f6735ca1016f9f3baffba
LW-Ho <leoleo41306@gmail.com>
2018-07-20 19:58:39 +0800

    check the simulator is cooja. or not, run uart.

--- projects/white_Testing/children/node.c
+++ projects/white_Testing/children/node.c
@@ -305,30 +305,26 @@ PROCESS_THREAD(node_process, ev, data)
 
   PROCESS_BEGIN();
 
-  #if CONTIKI_TARGET_COOJA
-    //PRINTF("Running Cooja mote.\n");
-  #else
-    uart_set_input(1, serial_line_input_byte);
-  #endif /* CONTIKI_TARGET_COOJA */
+    //uart_set_input(1, serial_line_input_byte);
   
   while(1) {
 
     //if(sht21.status(SENSORS_READY) == 0) {
 
-      PROCESS_WAIT_EVENT();
-      if(ev == serial_line_event_message) {
-      leds_toggle(LEDS_RED);
+      // PROCESS_WAIT_EVENT();
+      // if(ev == serial_line_event_message) {
+      // leds_toggle(LEDS_RED);
 
-      rxdata = data;
-      PRINTF("Data received over UART %s\n", rxdata);
+      // rxdata = data;
+      // PRINTF("Data received over UART %s\n", rxdata);
 
-      // the data send to node.c, then compress into packet.
-      collect_data_send(rxdata);
+      // // the data send to node.c, then compress into packet.
+      // collect_data_send(rxdata);
       
-      PRINTF("Received Done.\n");
-      }else {
-        PRINTF("Nothing... \n");
-      }
+      // PRINTF("Received Done.\n");
+      // }else {
+      //   PRINTF("Nothing... \n");
+      // }
     // } else {
     //   PROCESS_YIELD_UNTIL(etimer_expired(&etaa));
     //   etimer_reset(&etaa);
