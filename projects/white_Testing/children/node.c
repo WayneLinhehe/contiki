5b261de6808802ab7628c3013905d898506b3f0c
LW-Ho <leoleo41306@gmail.com>
2018-08-21 15:55:17 +0800

    update the code. add peek coap_payload into slipbrige.c

--- projects/white_Testing/children/node.c
+++ projects/white_Testing/children/node.c
@@ -45,6 +45,7 @@
 #include "contiki.h"
 #include "contiki-net.h"
 #include "rest-engine.h"
+#include "er-coap-engine.h" // peek coap_payload_length
 
 #include "net/rpl/rpl.h"
 #include "net/rpl/rpl-private.h"
@@ -154,6 +155,7 @@ extern struct tsch_asn_t tsch_current_asn;
 static void
 print_network_status(void)
 {
+  
   int i;
   uint8_t state;
   uip_ds6_defrt_t *default_route;
