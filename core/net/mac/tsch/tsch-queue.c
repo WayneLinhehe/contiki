c62885e7799b46b5a7b06a6a0d482f5ccae24f1c
LW-Ho <leoleo41306@gmail.com>
2018-12-05 18:11:19 +0800

    will crash, restore code.

--- core/net/mac/tsch/tsch-queue.c
+++ core/net/mac/tsch/tsch-queue.c
@@ -285,7 +285,7 @@ tsch_queue_add_packet(const linkaddr_t *addr, mac_callback_t sent, void *ptr)
             p->ptr = ptr;
             p->ret = MAC_TX_DEFERRED;
             p->transmissions = 0;
-            p->packet_queue_buffer = (uint8_t)ringbufindex_elements(&n->tx_ringbuf);
+            //p->packet_queue_buffer = (uint8_t)ringbufindex_elements(&n->tx_ringbuf);
 
             /* show queuebuf information. */
             uint8_t i;
