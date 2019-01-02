f5aa38a66a619be121d13e008d9ca8be912f3ada
LW-Ho <leoleo41306@gmail.com>
2019-01-02 18:52:55 +0800

    add queuebuf as constant in parent_path_cost.

--- core/net/mac/tsch/tsch-queue.c
+++ core/net/mac/tsch/tsch-queue.c
@@ -308,7 +308,8 @@ tsch_queue_add_packet(const linkaddr_t *addr, mac_callback_t sent, void *ptr)
                PRINTF("TSCH-queue: Traffic classes In TSCH queue frome attr : %02x\n" ,data_tcflow);
 
               #ifdef RPL_CALLBACK_MORE_PKTQUE 
-              if ((ringbufindex_elements(&n->tx_ringbuf))+1 % 8 == 0) {
+              uint8_t ringbufSize = ringbufindex_size(&n->tx_ringbuf)
+              if ((ringbufindex_elements(&n->tx_ringbuf))+1 % (ringbufSize/2) == 0) {
                 PRINTF("TSCH-queue: working RPL_CALLBACK_MORE_PKTQUE\n");
                 RPL_CALLBACK_MORE_PKTQUE();
               }
--- core/net/rpl/rpl-mrhof.c
+++ core/net/rpl/rpl-mrhof.c
@@ -133,6 +133,7 @@ static uint16_t
 parent_path_cost(rpl_parent_t *p)
 {
   uint16_t base;
+  uint16_t queuebuf;
 
   if(p == NULL || p->dag == NULL || p->dag->instance == NULL) {
     return 0xffff;
@@ -153,10 +154,12 @@ parent_path_cost(rpl_parent_t *p)
   }
 #else /* RPL_WITH_MC */
   base = p->rank;
+  queuebuf = ((uint16_t)p->dio_pktqubf)*25;
+  
 #endif /* RPL_WITH_MC */
 
   /* path cost upper bound: 0xffff */
-  return MIN((uint32_t)base + parent_link_metric(p), 0xffff);
+  return MIN((uint32_t)base + parent_link_metric(p) + queuebuf, 0xffff);
 }
 /*---------------------------------------------------------------------------*/
 static rpl_rank_t
